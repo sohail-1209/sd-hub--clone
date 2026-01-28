e5361b1ac437849764d7622bb9ed995be9bee134
sohail-1209 <sohailpashe@gmail.com>
2026-01-28 19:27:03 +0530

    courses

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -6,10 +6,10 @@ const Contact = () => {
 
       <div className="px-4 sm:px-8 lg:px-50 py-12 lg:py-20 flex flex-col gap-6 lg:gap-10">
         <h1 className="text-4xl sm:text-5xl lg:text-7xl text-[#343f5a]">
-          Contact Us
+          Courses
         </h1>
         <p className="text-[#aeb0b7] text-sm sm:text-base lg:text-xl">
-          Home – Contact us
+          Home – Courses
         </p>
       </div>
 
